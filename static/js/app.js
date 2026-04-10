1f97631d5ad4024de756c7b0f83873f149cdd3f8
Lee Juik <wndlrdl123@gmail.com>
2026-04-10 13:41:25 +0900

    feat: 갤러리 6번째 사진 삭제 (photo-06 제거)

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,6 @@ const GALLERY = [
     '/static/images/gallery/photo-03.jpg',
     '/static/images/gallery/photo-04.jpg',
     '/static/images/gallery/photo-05.jpg',
-    '/static/images/gallery/photo-06.jpg',
     '/static/images/gallery/photo-07.jpg',
     '/static/images/gallery/photo-09.jpg',
     '/static/images/gallery/photo-10.jpg',
